6330767c7f2fec152a495b934c0a56296912de80
Martin Hobert <admin@dosh.dk>
2015-08-26 21:16:33 +0200

    Added frontpage styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,22 @@
 /* jshint devel:true */
-console.log('Look at app/js/main.js');
+setMobileNav();
+
+$(window).scroll(function(){
+  var sticky = $('.navbar'),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
+
+function setMobileNav() {
+  var menu = $("#desktop-menu").html();
+  console.log(menu);
+  $("#mobile-menu").html(menu);
+}
+
+$(".mobile-nav").click(function() {
+  $("#mobile-menu").slideToggle("slow", function() {
+    //Stuff for when animation is done
+  });
+});
