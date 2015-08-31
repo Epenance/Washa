a779600ee0ef64a5f8c7c4471773651ea97ab919
Martin Hobert <admin@dosh.dk>
2015-08-31 23:03:15 +0200

    Added FAQ page

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,6 @@
 
 function setMobileNav() {
   var menu = $('#desktop-menu').html();
-  console.log(menu);
   $('#mobile-menu').html(menu);
 }
 
@@ -15,3 +14,16 @@ $('.mobile-nav').click(function() {
 $(function() {
   setMobileNav();
 });
+
+//Accordion addons for faq styling
+
+
+$('.panel-collapse').on('show.bs.collapse', function () {
+  var ele = $(this).parent().addClass("accordion-active");
+  console.log(ele);
+})
+
+$('.panel-collapse').on('hide.bs.collapse', function () {
+  var ele = $(this).parent().removeClass("accordion-active");
+  console.log(ele);
+})
