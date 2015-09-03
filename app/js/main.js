92609ee9e1fa7365f51a231d95809b01e9892b7e
Martin Hobert <admin@dosh.dk>
2015-09-03 12:35:37 +0200

    Added fixes from feedback 2, Smoothscroll (See index koncepted)

--- app/js/main.js
+++ app/js/main.js
@@ -13,15 +13,37 @@ $('.mobile-nav').click(function() {
 
 $(function() {
   setMobileNav();
+
+  $('#mobile-menu li a').click(function() {
+    console.log("Hello?");
+    $('#mobile-menu').slideToggle('slow', function() {
+      //Stuff for when animation is done
+    });
+  });
 });
 
 //Accordion addons for faq styling
-
-
 $('.panel-collapse').on('show.bs.collapse', function () {
   $(this).parent().addClass('accordion-active');
 });
 
 $('.panel-collapse').on('hide.bs.collapse', function () {
   $(this).parent().removeClass('accordion-active');
+});
+
+
+//Smooth scroll for menu links
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 60 //Adjust this number to fit the header height
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
